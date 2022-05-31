fa7892e6076432fe13825f8f78defcd9db63283a
Tihomir Tonov <tihomir.tonov@ampeco.com>
2022-05-31 10:51:48 +0300

    Handle missing missing card bin and last 4

--- src/Models/Card.php
+++ src/Models/Card.php
@@ -8,16 +8,21 @@ class Card
     public string $token;
     public string $firstSix;
     public string $lastFour;
-    public string $month;
-    public string $year;
+    public int $month;
+    public int $year;
 
     public function __construct(array $data)
     {
         $this->cardType = $data['cardbrand'];
         $this->token = $data['cardToken'];
-        $this->firstSix = $data['cardBin'];
+        $this->firstSix = $data['cardBin'] ?? $this->panPieces($data['pan'] ?? '')[0] ?? '';
         $this->month = intval(explode('.', $data['cardExpiry'])[0]);
         $this->year = intval(explode('.', $data['cardExpiry'])[1]);
-        $this->lastFour = $data['panLast4'];
+        $this->lastFour = $data['panLast4'] ?? $this->panPieces($data['pan'] ?? '')[1] ?? '';
+    }
+
+    private function panPieces(string $pan): array
+    {
+        return array_values(array_filter(explode('*', $pan)));
     }
 }
